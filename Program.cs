abf2599bcbacc93ab4189f0ffc7bd2acaf5c1ad4
roze <roze@PCROZE>
2021-09-24 09:21:51 +0200

    Release

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace SR0._01_Mediensteuerung
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("Seminarraum 0.01");
         }
     }
 }
